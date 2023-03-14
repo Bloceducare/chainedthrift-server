87554a0d10b928a038c5d06fd5f88d619b3122d5
Pelumi <fatolupelumi@gmail.com>
2023-03-14 12:46:34 +0100

    add new url

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import express from "express";
 import cors from "cors";
-const corsOption = {
-    origin: '*',
-    credentials: true,
-    optionsSuccessStatus: 200
-};
+// const corsOption = {
+//     origin: '*',
+//     credentials: true,
+//     optionsSuccessStatus: 200
+// };
 const app = express();
 
-// app.use(cors(
-//     {
-//         origin: 'http://localhost:3000',
-//         credentials: true,
-//         methods: 'POST',
-//         optionsSuccessStatus: 200,
-//     }
-// ));
+app.use(cors(
+    {
+        origin: 'https://chainedthrift.finance',
+        credentials: true,
+        methods: 'POST',
+        optionsSuccessStatus: 200,
+    }
+));
 app.use(express.json());
 
 export default app;
