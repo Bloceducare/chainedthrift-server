25dc2bd0e270679c93de503006dfc2fb31c38bfc
Pelumi <fatolupelumi@gmail.com>
2023-03-14 13:10:15 +0100

    add new url

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,19 @@ import cors from "cors";
 // };
 const app = express();
 
-app.use(cors(
-    {
-        origin: 'https://chainedthrift.finance',
-        credentials: true,
-        methods: 'POST',
-        optionsSuccessStatus: 200,
-    }
-));
+// app.use(cors(
+//     {
+//         origin: 'https://chainedthrift.finance',
+//         credentials: true,
+//         methods: 'POST',
+//         optionsSuccessStatus: 200,
+//     }
+// ));
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
 app.use(express.json());
 
 export default app;
